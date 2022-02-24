b28568de2f6a817847a9a249160680e9829e0c90
Shruthi Rao <raoshruthi2001@gmail.com>
2022-02-24 22:35:15 +0530

    Added exp5

---
+++ exp5/video_capture.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(r'C:\Users\student\Downloads\one-by-one-person-detection.mp4')
+cv2.namedWindow('Video', cv2.WINDOW_AUTOSIZE)
+
+while True:
+    ret_val, frame = cap.read()
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) == 27:
+        break
+vid.release()
+cv2.destroyAllWindows()
