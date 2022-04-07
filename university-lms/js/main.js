45763a0dcbadd84f7fe253fc2f3ddfe42619e07b
Shruthi Rao <raoshruthi2001@gmail.com>
2022-04-07 23:41:29 +0530

    Added university lms

---
+++ university-lms/js/main.js
@@ -0,0 +1,9 @@
+function StudentLogIn() {
+    let studentEmail = document.querySelector('[name="studentEmail"]').value;
+    alert('Welcome ' + studentEmail + ' !\nYou have been logged in successfully.')
+}
+
+function FacultyLogIn() {
+    let facultyEmail = document.querySelector('[name="facultyEmail"]').value;
+    alert('Welcome ' + facultyEmail + ' !\nYou have been logged in successfully.')
+}
